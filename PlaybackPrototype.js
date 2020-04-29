0e9251a9a7517f9b0793cd5e5f911202e26fd37a
Ubuntu <ubuntu@ip-172-31-86-87.ec2.internal>
2020-04-29 20:00:06 +0000

    Hook architecture complete

--- PlaybackPrototype.js
+++ PlaybackPrototype.js
@@ -1,25 +1,47 @@
-//NOTE: this prototype will probably end up being exported as a module down the line.
-var http = require('http') //Offers various http capabilities.
+// NOTE: this prototype will probably end up being exported as a module down the line.
+var http = require('http') // Offers various http capabilities.
 var https = require('https')
-var mysql = require('mysql') //DB driver.
+var mysql = require('mysql') // DB driver.
+var sorted = require('sorted-array-functions');
 var requests_cache = {}
 
 var base_request_time = 0
 var base_local_time = 0;
 
+var hooks = [] //
+
+const hook_events = {
+    PRE_REQUEST: 'pre-request',
+    REQUEST_CALLBACK: 'request callback',
+    POST_REQUEST: 'post-request'
+}
+
+function register_hook(priority, event_type, func ) {
+    sorted.add(hooks, { value: priority, event_type: event_type, func: func }, (a, b) => { return a.value - b.value; })
+}
+
 function pre_request_hook(options, editable_options, req_options) {
-    console.log("pre_request_hook")
-    console.log(options, editable_options, req_options)
+    for(let hook of hooks) {
+        if(hook.event_type === hook_events.PRE_REQUEST) {
+            hook.func()
+        }
+    }
 }
 
 function request_callback_hook(res) {
-    console.log("request_callback_hook")
-    console.log(res)
+    for(let hook of hooks) {
+        if(hook.event_type === hook_events.REQUEST_CALLBACK) {
+            hook.func()
+        }
+    }
 }
 
 function post_request_hook(req, options) {
-    console.log("post_request_hook")
-    console.log(req, options)
+    for(let hook of hooks) {
+        if(hook.event_type === hook_events.POST_REQUEST) {
+            hook.func()
+        }
+    }
 }
 
 //This function forgoes completing a task for ms_delay milliseconds. //For our purposes, it is used to space out requests.
@@ -45,28 +67,31 @@ function dispatch_request(options){
             req_headers[name_val[0]] = name_val[1];
     }
 
-    var req_options = {"host":"localhost", "setHost":false, "path":options.uri, "method":options.method, "headers":req_headers}
+    var req_options = {"host":"localhost", "setHost":false, "path":options.uri, "method":options.method, "headers":req_headers, "port":8080}
     var editable_options = {
         'secure': options.secure,
-        'reqbody': options.reqbody
+        'reqbody': options.reqbody,
+        'send_request': true
     }
     pre_request_hook(options, editable_options, req_options);
 
-    //Select the correct request class
-    var webreq = editable_options['secure'] ? https : http
-    var req = webreq.request(req_options, (res) => {
-        // Code for testing
-        // res.setEncoding("utf-8")
-        // res.on('data', (data)=>{/*console.log(data)*/})
-        // res.on('end', ()=>{console.log("Finished streaming data for request response.")})
+    if(editable_options.send_request === true) {
+        //Select the correct request class
+        var webreq = editable_options['secure'] ? https : http
+        var req = webreq.request(req_options, (res) => {
+            // Code for testing
+            // res.setEncoding("utf-8")
+            // res.on('data', (data)=>{/*console.log(data)*/})
+            // res.on('end', ()=>{console.log("Finished streaming data for request response.")})
 
-        request_callback_hook(res);
-    }) //Create the request
+            request_callback_hook(res);
+        }) //Create the request
 
-    if(editable_options['reqbody'] != "")
-        req.write(options.reqbody)
-        post_request_hook(req, options)
-        req.end()
+        if(editable_options['reqbody'] != "")
+            req.write(options.reqbody)
+            post_request_hook(req, options)
+            req.end()
+    }
 }
 
 //Connection settings like username, password, and etc.
