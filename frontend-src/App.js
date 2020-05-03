12ab1a317f0e6d75928960868499ce3a64cb60bc
Berin-Hadziabdic <45540078+Berin-Hadziabdic@users.noreply.github.com>
2020-05-02 17:19:05 -0700

    Update App.js

--- frontend-src/App.js
+++ frontend-src/App.js
@@ -5,21 +5,15 @@ import PlayBack from './Components/Playback/Playback';
 
 
 class App extends React.Component {
-  
-
-
- 
-
-  
- render() {
-
     
+ render() {
+  
   return (
     <div className="App">
       <PlayBack  navLinks={[{title:"Home",route:"/"},{title:"In Progress Jobs",route:"/inprogressjobs"},{title:"Completed Jobs",route:"/completedjobs"}]}/>
     </div>
   );
-}
+ }
 }
 
 export default App;
