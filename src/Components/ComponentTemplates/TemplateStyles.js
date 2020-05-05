ef5696dbd28c647d8ef9aeff1c517b5af4501adb
Berin <berin2@pdx.edu>
2020-05-05 15:11:14 -0700

    This is mostly a coding refactor for the styling of the app with some minor styling tweaks. I was in the process of moving all styles into TemplateStyles.js when I commited this sometime ago. TemplateStyles.js is a singular file that is responsible for defining bootstrap styles for our components, so that if we have 1,10, or 50 components of one or multiple types, all we must do is change its style in this file and it's done, as opposed to changing each component individually.

--- src/Components/ComponentTemplates/TemplateStyles.js
+++ src/Components/ComponentTemplates/TemplateStyles.js
@@ -2,9 +2,18 @@
 //I tossed in some styles as a quick fix, but this will be done throughly properly when
 // the time comes.
 class TemplateStyles {
-  static NavListButton = "btn btn-sm btn-primary";
-  static BackNavButton = "btn btn-sm btn-success";
+  static RouteButton = "btn btn-sm btn-primary";
+  static BackNavButton = "btn btn-sm btn-success col-3";
   static HTTPServiceButton = "btn btn-sm btn-info";
+  static NavBar = "navbar navbar-expand-xl fixed-top nav-bar-custom ";
+  static NavButton = "nav-button button";
+  static BackNavHeading = "col-9 generic-heading yellow";
+  static BackNavHeadingRow = "row justify-content-end align-items-center";
+  static NavHeadingButtons =
+    "col-12 col-md-6 row justify-content-start align-items-center";
+  static NavHeadingText =
+    "col-12 col-md-6 row justify-content-start align-items-center";
+  static customRow = "row row-custom";
 }
 
 export default TemplateStyles;
