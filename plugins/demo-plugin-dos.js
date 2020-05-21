a3e3f28f43eae67d6c7d7839ea87eda6df18f531
Christopher Douglass <christopher.d.douglass@gmail.com>
2020-05-20 19:14:58 -0700

    commented out useless tuff

--- plugins/demo-plugin-dos.js
+++ plugins/demo-plugin-dos.js
@@ -1,3 +1,3 @@
 var methods = require('../Playback.js');
 
-methods.register_hook(42069, methods.hook_events.POST_REQUEST, () => console.log('In plugin hook with priority 42069'))
+// methods.register_hook(429, methods.hook_events.POST_REQUEST, () => console.log('In plugin hook with priority 429'))
