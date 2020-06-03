37b749457cd9720c81b0b12e3b9734fba072811e
pmraiders <pmraiders@gmail.com>
2020-06-03 16:18:06 -0700

    Cleanup of files and script code
    Removed nginx config example since documentation should cover whats needed.
    
    Prettyfied the php code and fixed the glaring nasty sqli vuln.

--- Capture/traffic_capture.php
+++ Capture/traffic_capture.php
@@ -1,9 +1,9 @@
 <?php
 //Database configuration
-$servername = "localhost";
-$username = "traffic";
-$password = "12345";
-$database = "trafficDB";
+$servername	= "localhost";
+$username	= "traffic";
+$password	= "12345";
+$database	= "trafficDB";
 //End script configuration block
 
 //Connect to mysql, and cleanly handle any errors.
@@ -16,18 +16,18 @@ if ($conn->connect_error)
 echo "Connected successfully<br>";
 
 //define variables in the global scope for later use.
-$source_ip = "0.0.0.0";
-$secure = 0;
-$proto = "INVAL";
-$host = "invalid.capture.com";
-$uri = "/";
-$method = "FAIL";
-$requesttime = 1588046592.000;
-$headers = "";
+$source_ip	= "0.0.0.0";
+$secure		= 0;
+$proto		= "INVAL";
+$host		= "invalid.capture.com";
+$uri		= "/";
+$method		= "FAIL";
+$requesttime	= 1588046592.000;
+$headers	= "";
 
 foreach (getallheaders() as $name => $value)
 {
-	switch($name)
+	switch(strtolower($name))
 	{
 	case "tpt-ip":
 		$source_ip = $value;
@@ -52,21 +52,22 @@ foreach (getallheaders() as $name => $value)
 		$requesttime = $value;
 		break;
 	default:
-		$headers .= "$name:::::$value" . "\r\n";
+		$headers .= "$name" . "\r\n" . "$value" . "\r\n";
 	}
 }
 
 //if headers are empty, lets give it a default value
 if(empty($headers))
-	$headers = "INVALID:HEADERS";
+	$headers = "INVALID\r\nHEADERS\r\n";
 
-$job_id = "";
-$sql = "SELECT * from job;";
+$jobs = "";
+$sql = "SELECT * from jobs;";
 if($result = $conn->query($sql))
 {
 	while($row = $result->fetch_assoc())
 	{
-		if(	((!$row['jobStart'] && !$row['jobStop']) || ($row['jobStart'] < $requesttime && $row['jobStop'] > $requesttime)) &&
+		if(	($row['active'] == 1) && 
+			((!$row['jobStart'] && !$row['jobStop']) || ($row['jobStart'] < $requesttime && $row['jobStop'] > $requesttime)) &&
 			(!$row['secure'] || preg_match($row['secure'], $secure)) &&
 			(!$row['protocol'] || preg_match($row['protocol'], $proto)) &&
 			(!$row['host'] || preg_match($row['host'], $host)) && 
@@ -74,26 +75,36 @@ if($result = $conn->query($sql))
 			(!$row['method'] || preg_match($row['method'], $method)) && 
 			(!$row['sourceip'] || preg_match($row['sourceip'], $source_ip)))
 		{
-			if(!$job_id)
-				$job_id = $row['jobID'];
+			if(!$jobs)
+				$jobs = $row['jobID'];
 			else
-				$job_id .= "," . $row['jobID'];
+				$jobs .= "," . $row['jobID'];
 		}
 	}
 }
 
-if($job_id == "")
+if($jobs == "")
 {
 	$conn->close();
-	die();
+	die("Quitting capture: No job found<br>");
 }
 
 $post = file_get_contents('php://input');
 
+$requesttime	= $conn->real_escape_string($requesttime);
+$secure		= $conn->real_escape_string($secure);
+$proto		= $conn->real_escape_string($proto);
+$host		= $conn->real_escape_string($host);
+$uri		= $conn->real_escape_string($uri);
+$method		= $conn->real_escape_string($method);
+$headers	= $conn->real_escape_string($headers);
+$post		= $conn->real_escape_string($post);
+$source_ip	= $conn->real_escape_string($source_ip);
+$jobs		= $conn->real_escape_string($jobs);
 
 //Construct our sql query to log the request to the database.
-$sql = "INSERT INTO raw (utime, secure, protocol, host, uri, method, header, reqbody, sourceip, jobID) VALUES ($requesttime, '$secure', '$proto', '$host', '$uri', '$method', '$headers', '$post', '$source_ip', '$job_id');";
-//echo($sql);
+$sql = "INSERT INTO raw (utime, secure, protocol, host, uri, method, header, reqbody, sourceip, jobs) VALUES ($requesttime, '$secure', '$proto', '$host', '$uri', '$method', '$headers', '$post', '$source_ip', '$jobs');";
+//echo($sql . '<br>');
 //Execute the sql query and handle any errors.
 if(!$conn->query($sql))
 	echo("Query Error" . $conn->error . "<br>");
