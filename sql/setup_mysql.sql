6eb26ac103fe79ac33e93d8065aa816b956e2770
pmraiders <pmraiders@gmail.com>
2020-06-08 18:49:37 -0700

    Update setup_mysql.sql
    Fixed formatting

--- sql/setup_mysql.sql
+++ sql/setup_mysql.sql
@@ -81,16 +81,16 @@ CREATE TABLE headerrel (
 	FOREIGN KEY (headerID) REFERENCES headers(headerID)
 );
 
-CREATE VIEW v_record AS
-SELECT jobID AS JobID, r.recordID AS RecordID, utime AS UTime, secure AS Secure, protocol AS Protocol, host AS Host, uri AS URI, method AS Method, r.sourceip AS SourceIP, headers AS Headers, body AS Body
-FROM records AS r
-	JOIN protocols AS p ON r.protocol = p.protocolID
-	JOIN hosts AS h ON r.host = h.hostID
-	JOIN uris AS u ON r.uri = u.uriID
-	JOIN methods AS m ON r.method = m.methodID
-	JOIN sourceips AS s ON r.sourceip = s.sourceipID
-	JOIN jobrel AS j ON r.recordID = j.recordID
-	JOIN (SELECT myTable.recID AS recID, CONCAT(GROUP_CONCAT(CONCAT(myTable.hName, '\r\n'), myTable.hVal SEPARATOR '\r\n'), '\r\n') AS headers FROM (SELECT h.headerName AS hName, h.headerValue AS hVal, rel.recordID AS recID FROM headers AS h, headerrel AS rel WHERE h.headerID = rel.headerID) AS myTable GROUP BY myTable.recID) AS hr ON r.recordID = hr.recID
-;
+CREATE OR REPLACE VIEW v_record AS
+select r.recordID as id, r.utime as utime, r.secure as secure, p.protocolName as protocol, h.hostName as host, u.uriName as uri, m.methodName as method, headers as header, r.body as reqbody, s.sourceip as sourceip, j.jobID as jobid
+from records as r
+	join protocols as p on r.protocol = p.protocolID
+	join hosts as h on r.host = h.hostID
+	join uris as u on r.uri = u.uriID
+	join methods as m on r.method = m.methodID
+	join sourceips as s on r.sourceip = s.sourceipID
+	join jobrel as j on r.recordID = j.recordID
+	join (select myTable.recID as recID, CONCAT(GROUP_CONCAT(CONCAT(myTable.hName, '\r\n'), myTable.hVal SEPARATOR '\r\n'), '\r\n') AS headers FROM (select h.headerName as hName, h.headerValue as hVal, rel.recordID as recID from headers as h, headerrel as rel where h.headerID = rel.headerID) AS myTable group by myTable.recID) as hr on r.recordID = hr.recID
+	ORDER BY r.utime ASC;
 
 
