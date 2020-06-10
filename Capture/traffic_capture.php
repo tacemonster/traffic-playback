dc2072a1c1143ec91682a2508603f6980c59675e
pmraiders <pmraiders@gmail.com>
2020-06-09 17:33:36 -0700

    UI Capture Job Documentation & Revised capture script that inserts into the schema
    Adding UI Capture Job.md
    Updating traffic_capture.php to use the entire sql schema.

--- Capture/traffic_capture.php
+++ Capture/traffic_capture.php
@@ -23,7 +23,7 @@ $host		= "invalid.capture.com";
 $uri		= "/";
 $method		= "FAIL";
 $requesttime	= 1588046592.000;
-$headers	= "";
+$headers	= array();
 
 foreach (getallheaders() as $name => $value)
 {
@@ -52,15 +52,16 @@ foreach (getallheaders() as $name => $value)
 		$requesttime = $value;
 		break;
 	default:
-		$headers .= "$name" . "\r\n" . "$value" . "\r\n";
+		if(!empty($name) && !empty($value))
+			$headers[$name] = $value;
 	}
 }
 
 //if headers are empty, lets give it a default value
 if(empty($headers))
-	$headers = "INVALID\r\nHEADERS\r\n";
+	$headers[$name] = $value;
 
-$jobs = "";
+$jobs = array();
 $sql = "SELECT * from jobs;";
 if($result = $conn->query($sql))
 {
@@ -75,18 +76,28 @@ if($result = $conn->query($sql))
 			(!$row['method'] || preg_match($row['method'], $method)) && 
 			(!$row['sourceip'] || preg_match($row['sourceip'], $source_ip)))
 		{
-			if(!$jobs)
-				$jobs = $row['jobID'];
-			else
-				$jobs .= "," . $row['jobID'];
+			$jobs[] = $row['jobID'];
 		}
 	}
 }
 
-if($jobs == "")
+if(empty($jobs))
 {
 	$conn->close();
 	die("Quitting capture: No job found<br>");
+}
+
+function mysqli_upsert($sql_conn, $select_query, $insert_query)
+{
+	$result = $sql_conn->query($select_query);
+
+	if(!$result->num_rows)
+	{
+		$sql_conn->query($insert_query);
+		$result = $sql_conn->query($select_query);
+	}
+
+	return $result;
 }
 
 $post = file_get_contents('php://input');
@@ -97,17 +108,79 @@ $proto		= $conn->real_escape_string($proto);
 $host		= $conn->real_escape_string($host);
 $uri		= $conn->real_escape_string($uri);
 $method		= $conn->real_escape_string($method);
-$headers	= $conn->real_escape_string($headers);
 $post		= $conn->real_escape_string($post);
 $source_ip	= $conn->real_escape_string($source_ip);
-$jobs		= $conn->real_escape_string($jobs);
 
-//Construct our sql query to log the request to the database.
-$sql = "INSERT INTO raw (utime, secure, protocol, host, uri, method, header, reqbody, sourceip, jobs) VALUES ($requesttime, '$secure', '$proto', '$host', '$uri', '$method', '$headers', '$post', '$source_ip', '$jobs');";
-//echo($sql . '<br>');
-//Execute the sql query and handle any errors.
+//insert into all fields besides headers
+$sql = "INSERT IGNORE INTO protocols (protocolName) VALUES ('$proto');
+INSERT IGNORE INTO hosts (hostName) VALUES ('$host');
+INSERT IGNORE INTO uris (uriName) VALUES ('$uri');
+INSERT IGNORE INTO methods (methodName) VALUES ('$method');
+INSERT IGNORE INTO sourceips (sourceip) VALUES ('$source_ip');";
+
+echo $sql . "<br>";
+if(!$conn->multi_query($sql))
+	echo "Query Error: " . $conn->error . "<br>";
+
+do
+{
+	$conn->store_result();
+}
+while($conn->next_result());
+$sql = "SELECT protocols.protocolID, hosts.hostID, uris.uriID, methods.methodID, sourceips.sourceipID
+		FROM protocols, hosts, uris, methods, sourceips
+		WHERE protocolName = '$proto'
+		AND hostName = '$host'
+    		AND uriName = '$uri'
+    		AND methodName = '$method'
+		AND sourceip = '$source_ip';";
+
+$result = $conn->query($sql);
+
+if(!$result)
+	echo "Query Error: " . $conn->error . "<br>";
+$rc_ids = $result->fetch_array();
+$protocolID = $rc_ids['protocolID'];
+$hostID = $rc_ids['hostID'];
+$uriID = $rc_ids['uriID'];
+$methodID = $rc_ids['methodID'];
+$sourceipID = $rc_ids['sourceipID'];
+
+
+
+//insert into records
+$sql = "INSERT INTO records (utime, secure, protocol, host, uri, method, sourceip, body) VALUES ($requesttime, $secure, $protocolID, $hostID, $uriID, $methodID, $sourceipID, '$post');";
 if(!$conn->query($sql))
-	echo("Query Error" . $conn->error . "<br>");
+	echo("Query Error: " . $conn->error . "<br>");
+
+$sql = "SELECT LAST_INSERT_ID();";
+$result = $conn->query($sql);
+
+$recordID = $result->fetch_row()[0];
+
+
+//insert into jobrel
+
+foreach($jobs as $i => $job)
+{
+	$sql = "INSERT INTO jobrel (jobID, recordID) VALUES ($job, $recordID);";
+	$conn->query($sql);
+}
+//insert into headers and headerrel
+foreach($headers as $name => $value)
+{
+	$headerID = 0;
+
+	$safe_name = $conn->real_escape_string($name);
+	$safe_value = $conn->real_escape_string($value);
+	
+	$result = mysqli_upsert($conn, "SELECT headerID FROM headers h WHERE h.headerName = '$safe_name' AND h.headerValue = '$safe_value';", "INSERT IGNORE INTO headers (headerName, headerValue) VALUES ('$safe_name', '$safe_value');");
+
+	$headerID = $result->fetch_row()[0];
+
+	$sql = "INSERT INTO headerrel (recordID, headerID) VALUES ($recordID, $headerID)";
+	$conn->query($sql);
+}
 
 //Close the database connection and terminate the script.
 $conn->close();
